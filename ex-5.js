99d6cb12dfeba14fcb826c1d03f67a5aa41a2052
Thanakon <thanakon.piti@gmail.com>
2024-09-30 14:14:00 +0700

    feat: primitive value on ex1-ex8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPriceProduct = 27000;
+let discount = totalPriceProduct * 25/100;
+let realPrice = totalPriceProduct - discount;
+
+
+
--- ex-6.js
+++ ex-6.js
@@ -3,4 +3,8 @@
 let totalParts = 13000;
 
 let concatSentence;
+totalParts = "Total: " + totalParts + " Machine parts";
 let literalSentence;
+totalParts = `Total: ${totalParts} Machine parts`;
+console.log(totalParts);
+
